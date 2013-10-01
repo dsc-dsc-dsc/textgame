6f58f018dea9c87f3b45f10d88183879f9bdee43
Dash Scarborough <mcfundash@gmail.com>
2013-09-30 21:37:32 -0700

    more stuff

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import sys
 import os
 start = 0
-x = 5
-y = 4
-size = [73, 57]
+size = [16, 10]
+x = size[1]/2
+y = size[0]/2
+#x=1
+#y=1
 def genbox(size = size):
     size[0]=size[0]-2
     size[1]=size[1]-2
@@ -17,9 +19,9 @@ def genbox(size = size):
     return finbox
 line = genbox()
 def printline():
-    #os.system('clear')
-    print chr(27) + "[2J"
-    #sys.stderr.write("\x1b[2J\x1b[H")
+    os.system('clear')
+#    print chr(27) + "[2J"
+#    sys.stderr.write("\x1b[2J\x1b[H")
     for current in line:
         print current
     sys.stdout.flush()
@@ -53,31 +55,33 @@ def control(inp):
 #    inp = raw_input()
 #    inp = getch()
     print inp
-    if inp not in ["w", "a", "s", "d", "zz", "q"]:
+    if inp not in ["w", "a", "s", "d", "zz", "q", ""]:
         print "Invalid input"
     elif inp == "q":
         sys.exit(1)
         line = ['x','x']
         return 1
+    elif inp == "":
+        print "Press Q to quit."
     elif inp == "w":
         y-=1
-        if y < 0:
-            y = size[1] + 1
+        if y < 1:
+            y = size[1]
         lineman(x,y, "w")
     elif inp == "d":
         x+=1
-        if x > size[0] + 1:
-            x = 0
+        if x > size[0]:
+            x = 1
         lineman(x,y, "d")
     elif inp == "s":
         y+=1
-        if y > size[1] + 1:
-            y = 0
+        if y > size[1]:
+            y = 1
         lineman(x,y, "s")
     elif inp == "a":
         x-=1
-        if x < 0:
-            x = size[0] + 1
+        if x < 1:
+            x = size[0]
         lineman(x,y, "a")
 inp = "zz"
 control(inp)
